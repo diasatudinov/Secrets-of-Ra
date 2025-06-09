79e7899ca216b69aa3f8e4d1511f2dace2d1dfea
Dias <dias.atudinov@gmail.com>
2025-06-09 19:51:34 +0500

    add daily task view

--- Secrets of Ra/Secrets of Ra/SRModules/SRDailyTask/SRDailyTaskView.swift
+++ Secrets of Ra/Secrets of Ra/SRModules/SRDailyTask/SRDailyTaskView.swift
@@ -8,8 +8,100 @@
 import SwiftUI
 
 struct SRDailyTaskView: View {
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                HStack {
+                    HStack(alignment: .top) {
+                        
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                            
+                        } label: {
+                            Image(.backIconSR)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SaracenDeviceInfo.shared.deviceType == .pad ? 100:50)
+                        }
+                        
+                        
+                        
+                        Spacer()
+                        
+                        Image(.dailyTaskTextSR)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        Spacer()
+                        Image(.backIconSR)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: SaracenDeviceInfo.shared.deviceType == .pad ? 100:50)
+                            .opacity(0)
+                    }.padding([.top, .horizontal])
+                }
+                
+                VStack {
+                    
+                    ZStack {
+                        Image(.task1TextSR)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        VStack {
+                            Spacer()
+                            Image(.priceHundredSR)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SaracenDeviceInfo.shared.deviceType == .pad ? 100:50)
+                                .padding(.bottom, 30)
+                        }
+                        
+                    }.frame(height: SaracenDeviceInfo.shared.deviceType == .pad ? 300:150)
+                    
+                    ZStack {
+                        Image(.task2TextSR)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        VStack {
+                            Spacer()
+                            Image(.priceHundredSR)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SaracenDeviceInfo.shared.deviceType == .pad ? 100:50)
+                                .padding(.bottom, 30)
+                        }
+                        
+                    }.frame(height: SaracenDeviceInfo.shared.deviceType == .pad ? 300:150)
+                    
+                    ZStack {
+                        Image(.task3TextSR)
+                            .resizable()
+                            .scaledToFit()
+                        
+                        VStack {
+                            Spacer()
+                            Image(.priceHundredSR)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: SaracenDeviceInfo.shared.deviceType == .pad ? 100:50)
+                                .padding(.bottom, 30)
+                        }
+                        
+                    }.frame(height: SaracenDeviceInfo.shared.deviceType == .pad ? 300:150)
+                }
+                
+                Spacer()
+            }
+        }.background(
+            Image(.appBgSR)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .scaledToFill()
+        )
     }
 }
 
